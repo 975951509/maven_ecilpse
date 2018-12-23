d720f835c1e668ef2b9e0a584be7d2a92f0579f9
xiaochui <975951509@qq.com>
2018-12-23 18:08:55 +0800

    测试

--- xiaochui_ssm_service/src/main/java/cn/xiaochui/service/OrdersService.java
+++ xiaochui_ssm_service/src/main/java/cn/xiaochui/service/OrdersService.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface OrdersService {
     public List<Orders> findAll( int page,int size);
     public Orders findById(String id);
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
 }
